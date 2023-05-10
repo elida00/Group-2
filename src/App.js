5179404a31ddbf1c2d1376d0d603fbd60fdb4e3f
elida00 <you@example.com>
2023-05-10 16:50:35 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import {FuterPage} from './components/Futer'
+
+export const App=()=> {
+  return (
+    <div className="App">
+      <FuterPage/>
+    </div>
+  );
+}
+
+
---
+++ src/components/Futer/index.jsx
@@ -0,0 +1,72 @@
+import React from "react";
+import css from './Futer.module.css'
+// import mypicone from '../../images/1.png'
+export const FuterPage =() =>{
+    return(
+        <div className={css.futerPage}>
+{/*---------------------------------------------------- */}
+            <div className={css.divone}>
+                <span className={css.textone}>Deation & Evaluation - Best Way 
+                <br/>to Kick off Your Product Idea</span>
+            </div>
+{/* --------------------------------------------------- */}
+            <div>
+                <span className={css.texttwo}>Product <br></br> Design Sprint</span>
+                <p className={css.textthree}>A five-day workshop that<br></br> will help you answer crucial <br></br>business questions</p>
+                <h3 className={css.textfour}>  Read More</h3>
+            </div>
+            <div>
+                <span className={css.textfife}>Scoping<br></br> Session</span>
+                <span  className={css.textsix}>UX<br></br> Review</span>
+                <span className={css.textseven}>Code<br></br> Review</span>
+            </div>
+            <div>
+                <span className={css.texteght}>A workshop aimed at <br></br>shaping your business <br></br>idea, answering questions <br></br>regarding project planning</span>
+                <span className={css.textnine}>An evaluation which will <br></br>help you radically improve<br></br> your product.</span>
+                <span className={css.textten}>Do you know what one of <br></br>the key secrets of success <br></br>is what makes people<br></br> successful in business</span>
+            </div>
+            <div>
+                <span className={css.texttwenty}>  Read More</span>
+                <span className={css.texttreety}>  Read More</span>
+                <span className={css.textfourty}>  Read More</span>
+            </div>
+{/* ----------------------------------------------------- */}
+{/* ------------------------------------------------------- */}
+             <div className={css.texteghty}>
+                <span className={css.textninety}>Phone</span>
+                <span className={css.texthandred}>+32 50 31 28 32</span>
+                <span className={css.one}>Address</span>
+                <span className={css.two}>491 Merlin Crest Suite 963</span>
+                {/* --------- */}
+                <div className={css.abs}></div>
+                {/* --------- */}
+                <span className={css.three}>Services</span>
+                <span className={css.four}>Help And Advice</span>
+                <span className={css.fife}>Company</span>
+                <span className={css.six}>Get in Touch</span>
+                {/* --------- */}
+                <span className={css.seven}>Strategy Design <br></br> Product Design<br></br> Content Strategy<br></br> Brand Strategy <br></br>Development </span>
+                <span className={css.eight}>How it works <br></br>Contact Support<br></br> Privacy Policy <br></br>FAQ</span>
+                <span className={css.nine}>About<br></br> Blog<br></br> Contact <br></br>Jobs</span>
+                <span className={css.ten}>Feel free to get in touch with us <br></br>vai email</span>
+                {/* ---------- */}
+                <span className={css.ones}>info.webovio@gmail.com</span>
+                {/* <img src={mypicone}></img> */}
+                {/* ---------- */}
+                 {/* <img src="" alt="" /> */}
+                <div className={css.absd}></div>
+                 <span className={css.tens}>© 2020@webovio. All Rights Reserved.</span>
+                 <span className={css.lll}>webovio</span>
+             </div>         
+                {/* ----------------------------------------------------- */}
+             <div className={css.textseventy}>
+                 <span className={css.textfifety}>Let’s make something<br></br> amazing together.</span>
+                 <p className={css.textsixty}>  LET'S GET STARTED</p>
+            </div>
+    
+    
+    
+            
+        </div>
+    )
+}
